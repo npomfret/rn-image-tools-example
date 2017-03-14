a1f9abc138304dc3379a28c324f2e1d12668dbe9
npomfret <nick-github@snowmonkey.co.uk>
2017-03-14 18:40:19 +0000

    android setup

--- RNImageToolsExample/android/app/src/main/java/com/rnimagetoolsexample/MainApplication.java
+++ RNImageToolsExample/android/app/src/main/java/com/rnimagetoolsexample/MainApplication.java
@@ -1,42 +1,44 @@
 package com.rnimagetoolsexample;
 
 import android.app.Application;
+import android.support.multidex.MultiDex;
 
 import com.facebook.react.ReactApplication;
-import com.reactlibrary.RNImageToolsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.reactlibrary.RNImageToolsPackage;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+            return Arrays.<ReactPackage>asList(
+                    new MainReactPackage(),
+                    new RNImageToolsPackage()
+            );
+        }
+    };
+
     @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
     }
 
     @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNImageToolsPackage()
-      );
+    public void onCreate() {
+        super.onCreate();
+        MultiDex.install(getBaseContext());
+        SoLoader.init(this, /* native exopackage */ false);
     }
-  };
-
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-  }
 }
