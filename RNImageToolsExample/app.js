f1a4b9e650fb8a094684ce43fcc86855b3cdffcc
npomfret <nick-github@snowmonkey.co.uk>
2017-03-14 17:14:53 +0000

    open gellery

--- RNImageToolsExample/app.js
+++ RNImageToolsExample/app.js
@@ -1,20 +1,61 @@
 "use strict";
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, {Component} from "react";
+import {AppRegistry, StyleSheet, Text, View, NativeModules, Button, Image} from "react-native";
 
 export default class RNImageToolsExample extends Component {
+  constructor(props) {
+    super(props);
+    this._openGallery = this._openGallery.bind(this);
+    this._openEditor = this._openEditor.bind(this);
+    this.state = {
+      originalImageUri: null
+    }
+  }
+
+  async _openGallery() {
+    const uri = await NativeModules.RNImageTools.openGallery();
+    console.log("selected", uri);
+    this.setState({originalImageUri: uri});
+  }
+
+  _openEditor() {
+    NativeModules.RNImageTools.openEditor(this.state.originalImageUri)
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           react-native image tools example
         </Text>
+
+        <View style={{marginVertical: 4}}>
+          <Button
+            onPress={this._openGallery}
+            title="open gallery"
+            color="#841584"
+          />
+        </View>
+
+        {
+          this.state.originalImageUri ? <View>
+              <View style={{marginVertical: 4, flexDirection: 'row', justifyContent: 'center'}}>
+                <View style={{borderWidth: 1, borderRadius: 4, borderColor: 'blue'}}>
+                  <Image style={{width: 64, height: 64}} source={{uri: this.state.originalImageUri}}/>
+                </View>
+              </View>
+
+              <View style={{marginVertical: 4}}>
+                <Button
+                  onPress={this._openEditor}
+                  title="open editor"
+                  color="#841584"
+                />
+              </View>
+
+            </View> : null
+        }
       </View>
     );
   }
