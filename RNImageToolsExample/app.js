f6505458649800b688117e7dab4615b07e8f38bd
npomfret <nick-github@snowmonkey.co.uk>
2017-04-02 18:38:09 +0100

    added metadata

--- RNImageToolsExample/app.js
+++ RNImageToolsExample/app.js
@@ -1,7 +1,7 @@
 "use strict";
 
 import React, {Component} from "react";
-import {AppRegistry, StyleSheet, Text, View, Button, Image, Dimensions, CameraRoll, Platform, TextInput, StatusBar} from "react-native";
+import {AppRegistry, StyleSheet, Text, View, Button, Image, Dimensions, CameraRoll, Platform, TextInput, StatusBar, ScrollView, TouchableOpacity} from "react-native";
 import RNImageTools from "react-native-image-tools";
 
 const _width = Dimensions.get('window').width;
@@ -14,6 +14,7 @@ export default class RNImageToolsExample extends Component {
     this._openEditor = this._openEditor.bind(this);
 
     this.state = {
+      selectedImage: null,
       originalImageUri: null,
       editedImageUri: null,
       outputFormat: 'JPEG',
@@ -30,18 +31,19 @@ export default class RNImageToolsExample extends Component {
       "client-redirect-here"
     );
 
-    let originalImageUri  = await this.pickAnImageFromPhotos();
-    if(!originalImageUri) {
+    let originalImageUri = await RNImageToolsExample.pickAnImageFromPhotos();
+    if (!originalImageUri) {
       originalImageUri = "https://exposingtheinvisible.org/ckeditor_assets/pictures/32/content_example_ibiza.jpg";//some image that has metadata
     }
 
     this.setState({
       originalImageUri: originalImageUri,
+      selectedImage: originalImageUri,
       editedImageUri: null
     });
   }
 
-  async pickAnImageFromPhotos() {
+  static async pickAnImageFromPhotos() {
     const fetchParams = {
       first: 1,
       groupTypes: "SavedPhotos",
@@ -89,7 +91,7 @@ export default class RNImageToolsExample extends Component {
 
       console.log("edited uri", uri);
 
-      if(!uri) {
+      if (!uri) {
         console.log("editing cancelled");
       } else {
         this.setState({editedImageUri: uri});
@@ -101,8 +103,8 @@ export default class RNImageToolsExample extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <StatusBar hidden={true} />
+      <ScrollView contentContainerStyle={styles.container}>
+        <StatusBar hidden={true}/>
 
         <Text style={styles.welcome}>
           react-native image tools example
@@ -119,9 +121,11 @@ export default class RNImageToolsExample extends Component {
         {
           this.state.originalImageUri ? <View>
               <View style={{marginVertical: 4, flexDirection: 'row', justifyContent: 'center'}}>
-                <View style={{borderWidth: 1, borderRadius: 4, borderColor: 'blue'}}>
-                  <Image style={{width: _width / 2, height: _width / 2}} source={{uri: this.state.originalImageUri}}/>
-                </View>
+                <TouchableOpacity onPress={() => this.setState({selectedImage: this.state.originalImageUri})}>
+                  <View style={{borderWidth: 1, borderRadius: 4, borderColor: 'blue'}}>
+                    <Image style={{width: _width / 2, height: _width / 2}} source={{uri: this.state.originalImageUri}}/>
+                  </View>
+                </TouchableOpacity>
               </View>
             </View> : null
         }
@@ -129,7 +133,7 @@ export default class RNImageToolsExample extends Component {
         <View style={{flexDirection: 'row'}}>
           <View style={{marginVertical: 4, flex: 1}}>
             <TextInputField label="input image" value={this.state.originalImageUri} onChangeText={(value) => this.setState({originalImageUri: value})}/>
-            <TextInputField label="output quality" value={this.state.quality.toString()} onChangeText={(value) => this.setState({quality: value})} />
+            <TextInputField label="output quality" value={this.state.quality.toString()} onChangeText={(value) => this.setState({quality: value})}/>
             <TextInputField label="output format" value={this.state.outputFormat} onChangeText={(value) => this.setState({outputFormat: value})}/>
           </View>
         </View>
@@ -148,12 +152,16 @@ export default class RNImageToolsExample extends Component {
 
         {
           this.state.editedImageUri ? <View style={{marginVertical: 4, flexDirection: 'row', justifyContent: 'center'}}>
-              <View style={{borderWidth: 1, borderRadius: 4, borderColor: 'blue'}}>
-                <Image style={{width: _width / 2, height: _width / 2}} source={{uri: this.state.editedImageUri}}/>
-              </View>
+              <TouchableOpacity onPress={() => this.setState({selectedImage: this.state.editedImageUri})}>
+                <View style={{borderWidth: 1, borderRadius: 4, borderColor: 'blue'}}>
+                  <Image style={{width: _width / 2, height: _width / 2}} source={{uri: this.state.editedImageUri}}/>
+                </View>
+              </TouchableOpacity>
             </View> : null
         }
-      </View>
+
+        <MetadataView image={this.state.selectedImage}/>
+      </ScrollView>
     );
   }
 }
@@ -169,11 +177,72 @@ class TextInputField extends Component {
   }
 }
 
+class MetadataView extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      metadata: {}
+    }
+  }
+
+  componentDidMount() {
+    this._update();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.image !== prevProps.image) {
+      this._update();
+    }
+  }
+
+  async _update() {
+    const imageUri = this.props.image;
+    if (!imageUri) {
+      return;
+    }
+
+    try {
+      const metadata = await RNImageTools.imageMetadata(imageUri);
+      this.setState({metadata});
+      console.log("metadata", metadata);
+    } catch (e) {
+      console.log("failed to get image metadata", e);
+    }
+  }
+
+  _format(metadata, parent) {
+    const arr = [];
+    for (let key in metadata) {
+      const value = metadata[key];
+      const type = typeof value;
+      const isArray = (value instanceof Array);
+      const isObject = type === 'object' && !isArray;
+
+      arr.push(
+        <View key={parent + "_" + key} style={{flexDirection: 'row'}}>
+          <Text style={{fontSize: 10, color: "#d1d2cd"}}>{key}</Text>
+          {
+            isObject ? this._format(value, key) : <Text key={key} style={{fontSize: 10, color: "#d1d2cd"}}>: {JSON.stringify(value)}</Text>
+          }
+        </View>
+      );
+    }
+    return <View style={{padding: 4}}>{arr}</View>;
+  }
+
+  render() {
+    return <View style={{flex: 1, backgroundColor: '#666666'}}>
+      <Text>Image metadata</Text>
+      {this._format(this.state.metadata, 'root')}
+    </View>
+  }
+}
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#e4ffe2',
-    paddingHorizontal: 8
+    padding: 8
   },
   welcome: {
     fontSize: 14,
